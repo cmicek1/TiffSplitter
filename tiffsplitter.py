819f8468ff937e2d46c467eabec378601b8a8ada
cmicek1 <cmicek325@verizon.net>
2016-06-06 16:25:10 -0400

    Updated naming convention, documentation.

--- tiffsplitter.py
+++ tiffsplitter.py
@@ -1,12 +1,22 @@
+import os
 import tifffile as tf
-import Tkinter as tk
+import Tkinter as Tk
 import tkFileDialog
 
 
 class TiffSplitter:
+    """
+    Utility for splitting 3D TIFF files into individual TIFF images.
+    Meant for use with the Open Connectome Project's NeuroDataViz tool.
+    """
     def __init__(self, location, destination=None):
         """
+        Constructor; creates splitting utility.
 
+        :param location: file path of TIFF stack to split
+        :param destination: destination file path; default is location
+                            of stack
+        :type location: str
         :type destination: str
         """
         self.location = location
@@ -16,13 +26,20 @@ class TiffSplitter:
             self.destination = destination
 
     def split(self):
+        """
+        Splits a TIFF stack into individual images, and outputs them to
+        destination. The naming format is according to NeuroData specs,
+        with images named in numerical order from '0000' to 'X'.
+        :rtype: None
+        """
         image = tf.TiffFile(self.location)
         for i in range(len(image.pages)):
-            tf.imsave("{0}_{1}.tif".format(self.destination.split(".tif")[0], str(i)), image.pages[i].asarray())
+            tf.imsave("{0}\{1:04d}.tif".format(
+                os.path.dirname(self.location), i), image.pages[i].asarray())
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    root = Tk.Tk()
     root.withdraw()
     fpath = tkFileDialog.askopenfilename()
     ts = TiffSplitter(fpath)
