929b96705dfcf434677b7b5cc2152ace23a1c5f3
cmicek1 <cmicek325@verizon.net>
2016-06-07 17:42:57 -0400

    Added resizing functionality (still buggy), stable loop.

--- viewer.py
+++ viewer.py
@@ -1,28 +1,43 @@
 import pygame as pg
 import tiffstack as ts
+import Tkinter as Tk
+import tkFileDialog
+
+BIT_DEPTH = 8
 
 
 class Viewer:
     def __init__(self, stack, caption="Stack Browser"):
-        pg.init()
-        self.screen = pg.display.set_mode(pg.display.list_modes()[0],
-                                          pg.RESIZABLE)
-        pg.display.set_caption(caption)
         self.stack = stack
-
+        pg.init()
         imarray = self.stack.getarray
         sz_to_use = tuple([imarray.shape[1], imarray.shape[2]])
-        if self.screen.get_size() != sz_to_use:
-            pg.display.set_mode(sz_to_use, 0, 8)
-            pg.display.flip()
+        self.screen = pg.display.set_mode(sz_to_use, pg.RESIZABLE,
+                                          BIT_DEPTH)
+        pg.display.set_caption(caption)
 
         self.background = pg.Surface(self.screen.get_size())
         self.background = self.background.convert()
-        pg.surfarray.blit_array(self.background, imarray[0])
+        pg.surfarray.blit_array(self.background, imarray[20])
         self.screen.blit(self.background, (0, 0))
         pg.display.flip()
 
 
+def main():
+    root = Tk.Tk()
+    root.withdraw()
+    fpath = tkFileDialog.askopenfilename()
+    t = ts.TiffStack(fpath)
+    v = Viewer(t)
+    while 1:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.display.quit()
+                quit(0)
+
+        v.screen.blit(v.background, (0, 0))
+        pg.display.flip()
+
+
 if __name__ == '__main__':
-    t = ts.TiffStack("C:\Users\Chris\Desktop\Vascular-Data\X20140523_a153_001_ch2.tif")
-    Viewer(t)
+    main()
