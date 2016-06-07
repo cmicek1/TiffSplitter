28a5d89c9720e7adb57955a4601f281358865e84
Chris Micek <cmicek325@verizon.net>
2016-06-06 20:14:27 -0400

    Update - displays a TIFF image successfully!

--- Viewer.py
+++ Viewer.py
@@ -1,6 +1,6 @@
 import pygame as pg
-import numpy as np
-import TiffStack as ts
+import tiffstack as ts
+
 
 class Viewer:
     def __init__(self, stack, caption="Stack Browser"):
@@ -8,9 +8,16 @@ class Viewer:
         pg.display.set_mode(pg.display.list_modes()[0], pg.RESIZABLE)
         pg.display.set_caption(caption)
         self.stack = stack
+        self.screen = pg.display.get_surface()
 
-        imarray = self.stack.image.asarray()
-        pg.surfarray.blit_array(pg.display.get_surface(), imarray[1])
+        imarray = self.stack.getarray
+        sz_to_use = tuple([imarray.shape[1], imarray.shape[2]])
+        if self.screen.get_size() != sz_to_use:
+            pg.display.set_mode(sz_to_use, 0, 8)
+            pg.display.flip()
+        pg.surfarray.blit_array(self.screen, imarray[0])
 
 
-
+if __name__ == '__main__':
+    t = ts.TiffStack("C:\Users\Chris\Desktop\Vascular-Data\X20140523_a153_001_ch2.tif")
+    Viewer(t)
