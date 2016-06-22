3c41f812e8a79b138d2ad78a13895558dcd67d55
cmicek1 <cmicek325@verizon.net>
2016-06-22 15:51:49 -0400

    Directory format fixed.

--- tiffsplitter.py
+++ tiffsplitter.py
@@ -4,57 +4,50 @@ import Tkinter as Tk
 import tkFileDialog
 
 
-class TiffSplitter:
+DIR_FORMAT = 'raw/channels8'
+
+
+def split(fpath, destination):
     """
-    Utility for splitting 3D TIFF files into individual TIFF images.
-    Meant for use with the Open Connectome Project's NeuroDataViz tool.
+    Splits a TIFF stack into individual images, and outputs them to
+    destination. The naming format is according to NeuroData specs,
+    with images named in numerical order from '0000' to 'X'.
+
+    :param fpath: File path of TIFF stack to split (eg a153_hs3)
+    :param destination: Destination file path; default is location
+                        of stack
+
+    :type fpath: str
+    :type destination: str
+
+
+    :rtype None
     """
-    def __init__(self, location):
-        """
-        Constructor; creates splitting utility.
-
-        :param location: directory path of TIFF stacks to split
-
-        :type location: str
-        :type destination: str
-        """
-        self.location = location
-
-    def split(self, fpath, destination):
-        """
-        Splits a TIFF stack into individual images, and outputs them to
-        destination. The naming format is according to NeuroData specs,
-        with images named in numerical order from '0000' to 'X'.
-
-        :param fpath: file path of TIFF stack to split
-        :param destination: destination file path; default is location
-                            of stack
-
-        :type fpath: str
-        :type destination: str
+    image = tf.TiffFile(fpath)
+    for i in range(len(image.pages)):
+        if not os.path.exists(destination):
+            os.makedirs(destination)
+        tf.imsave("{0}/{1:04d}.tif".format(
+            destination, i),
+            image.pages[i].asarray())
 
 
-        :rtype None
-        """
-        image = tf.TiffFile(fpath)
-        for i in range(len(image.pages)):
-            if not os.path.exists(destination):
-                os.makedirs(destination)
-            tf.imsave("{0}/{1:04d}.tif".format(
-                destination, i),
-                image.pages[i].asarray())
-
-
-if __name__ == "__main__":
+def main():
     root = Tk.Tk()
     root.withdraw()
+    start_dir = os.path.expanduser('~/Desktop/')
     cd = tkFileDialog.askdirectory(
-        initialdir=os.path.expanduser('~/Desktop'))
-    ts = TiffSplitter(cd)
+        initialdir=start_dir)
+    filepath = start_dir + '/' + os.path.basename(cd)
+    hstackname = os.path.dirname(filepath)
+    filepath += '/' + DIR_FORMAT
     time = 0
-    for item in os.listdir(ts.location):
+    for item in os.listdir(filepath):
         if item.endswith("ch1.tif"):
-            path = '{0}/{1}'.format(ts.location, item)
-            ts.split(path, '{0}/Channel/time{1}'.format(path.split(
-                '.')[0].split('_', 1)[1], time))
+            path = '{0}/{1}'.format(filepath, item)
+            split(path, '{0}/Channel/time{1}'.format(cd, time))
+            print "Creating {0}: time{1}".format(hstackname, time)
             time += 1
+
+if __name__ == '__main__':
+    main()
